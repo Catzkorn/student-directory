3181e91c495f87665b1add3af77b8d01fa2f44af
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-08-24 14:27:16 +0100

    Added ability to add students to array manually

--- directory.rb
+++ directory.rb
@@ -1,25 +1,44 @@
+=begin
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffery Baratheon",
-  "Norman Bates",
+  { name: "Dr. Hannibal Lecter", cohort: :november },
+  { name: "Darth Vader", cohort: :november },
+  { name: "Nurse Ratched", cohort: :november },
+  { name: "Michael Corleone", cohort: :november },
+  { name: "Alex DeLarge", cohort: :november },
+  { name: "The Wicked Witch of the West", cohort: :november },
+  { name: "Terminator", cohort: :november },
+  { name: "Freddy Krueger", cohort: :november },
+  { name: "The Joker", cohort: :november },
+  { name: "Joffery Baratheon", cohort: :november },
+  { name: "Norman Bates", cohort: :november },
 ]
+=end
+
+def input_students
+  students = []
+
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  name = gets.chomp
+
+  while !name.empty?
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "------------------"
 end
 
-def student_list(names)
-  names.each { |name|
-    puts name
+def student_list(students)
+  students.each { |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
@@ -27,6 +46,7 @@ def print_footer(names)
   print "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 student_list(students)
 print_footer(students)
